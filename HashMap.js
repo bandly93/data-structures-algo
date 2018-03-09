46d3a47687fd753d0cbeb239eff180deeed3a35d
Band Ly <bandly93@gmail.com>
2018-03-09 14:12:14 -0800

    first commit

---
+++ HashMap.js
@@ -0,0 +1,46 @@
+// write a simple hash table for quick lookups and insertions.
+
+//hash table constructor
+const HashTable = (size) => {
+	this.table = {};
+	this.numberOfValues = 0;
+	this.size = size;
+}
+
+//create an instance of the hashclass with a array size of 30;
+let myHash = new HashTable(30);
+
+//generates a hash code
+HashTable.prototype.generateHash = (key) => {
+	let total = 0;
+	for(let i = 0; i < key.length; i++){
+		total += key.charCodeAt(i);
+	}
+	let hash = total % this.size;
+	return hash;
+}
+
+//create insertion function and make sure to check collision 
+
+HashTable.prototype.add = (key,value) => {
+	let index = this.generateHash(key);
+	if(!this.table.hasOwnProperty(index)){
+		this.table[index] = {}
+	}
+	if(!this.table[index].hasOwnProperty(key)){
+		this.numberOfValues++;
+	}
+	this.table[index][key] = value;
+}
+
+//create search function
+HashTable.prototype.search = (key) => {
+	let index = this.generateHash(key);
+	if(this.values.hasOwnProperty(index) && this.value[hash].hasOwnProperty(key){
+		return this.value[index][key];
+	} else { 
+		return null;
+	}
+}
+
+
---
+++ SinglyLinkedList.js
@@ -0,0 +1,9 @@
+const Node = (data) => {
+	this.data = data;
+	this.next = null;
+}
+
+const SinglyList = () => {
+	this.length = 0;
+	this.head = null;
+}
