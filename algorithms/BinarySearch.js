9b90b27b80fa424a46b4c699c8eaf0fda1a766bc
Band Ly <bandly93@gmail.com>
2018-03-16 15:43:44 -0700

    finish quicksort-2

--- algorithms/BinarySearch.js
+++ algorithms/BinarySearch.js
@@ -5,8 +5,12 @@
 let array = [1,2,3,4,5,6,7,8,9,10];
 let target = '3';
 
-function BinarySort (array,target){
+function BinarySearch (array,target){
 	let half = Math.floor(array.length/2);
+	//first sort the array;
+	
+	
+	
 	
 	
 	
